4e2066ec21236f6327d86714fb89fd9dbcaaf295
decitre <590094+decitre@users.noreply.github.com>
2022-04-03 09:53:03 +0200

    Change the branch name in the build workflow

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ import setuptools
 
 
 project_name = "discogs_track"
-setuptools.setup(
-    name=project_name,
-    version=discogs_track.__version__,
-    license_files=("LICENSE",),
-    packages=setuptools.find_packages(),
-    package_data={project_name: ["py.typed"]},
-    entry_points={"console_scripts": [f"{project_name}={project_name}:cli"]},
-    python_requires=">=3.6",
-    zip_safe=False,
-)
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name=project_name,
+        version=discogs_track.__version__,
+        license_files=("LICENSE",),
+        packages=setuptools.find_packages(),
+        package_data={project_name: ["py.typed"]},
+        entry_points={"console_scripts": [f"{project_name}={project_name}:cli"]},
+        python_requires=">=3.6",
+        zip_safe=False,
+    )
